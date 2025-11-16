980b9677cc33519de023d40727faf3178f4c9408
Kinguslug <107000770+Kinguslug@users.noreply.github.com>
2025-11-16 14:41:23 -0500

    first try

--- diamond/Diamond.java
+++ diamond/Diamond.java
@@ -1,5 +1,35 @@
+import java.util.Scanner;
+
 public class Diamond {
   public static void main(String[] args) {
-    System.out.println("hello World");
+    Scanner scanner = new Scanner(System.in);
+
+    System.out.print("Enter a string:");
+    int input = scanner.nextInt();
+
+    if (input % 2 == 0) {
+      System.out.println("even");
+    } else {
+      for (int j = input / 2, i = 0; i != (input/2); i++, j--){
+        int spaceCounter = j;
+        int asteriskCounter = (2*i)+1;
+
+        if (i == (input/2)-1){
+          System.out.println("ahhhh broke");
+        }else{
+          while (spaceCounter-- > 0){
+            System.out.print(" ");
+        }
+
+          while (asteriskCounter-- > 0){
+            System.out.print("*");
+        }
+          System.out.print("\n");
+        }
+
+      }
+    }
+
+    scanner.close();
   }
 }
