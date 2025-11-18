872c81c0d375595f9282bc4008c8085522ee4135
Kinguslug <107000770+Kinguslug@users.noreply.github.com>
2025-11-18 12:37:56 -0500

    matrix done

--- matrix/Matrix.java
+++ matrix/Matrix.java
@@ -1,2 +1,62 @@
 public class Matrix {
+  private int[][] matrix;
+
+  public Matrix(int size) {
+
+    matrix = new int[size][size];
+
+  }
+  public void printDefultMatrix() {
+    for (int row = 0; row < matrix.length; row++) {
+      for (int col = 0; col < matrix[row].length; col++) {
+        System.out.print (matrix[row][col] + "\t");
+      }
+      System.out.println();
+    }
+  }
+  public void populateMatrix() {
+    int counter = 1;
+    for (int row = 0; row < matrix.length; row++) {
+      for (int col = 0; col < matrix[row].length; col++) {
+        matrix[row][col] = counter;
+        counter++;
+      }
+    }
+  }
+  public void printMatrix() {
+    int size = matrix.length;
+    for(int row = 0; row < size; row++) {
+      for (int col = 0; col < size; col++) {
+        if ( row + col == size - 1) {
+          System.out.print("\u001B[33m" + matrix[row][col] + "\u001B[0m" + "\t");
+        }else {
+          System.out.print(matrix[row][col] + "\t");
+        }
+      }
+      System.out.println();
+    }
+  }
+  private void swap(int x1, int y1, int x2, int y2) {
+    int temp = matrix[x1][y1];
+    matrix[x1][y1] = matrix[x2][y2];
+    matrix[x2][y2] = temp;
+  }
+  public void flipMatrix() {
+    int size = matrix.length;
+
+    for (int row = 0; row < size; row++) {
+      for (int col = 0; col < size; col++) {
+        if (row + col == size - 1) {
+          continue;
+        }
+
+        if (row + col < size - 1) {
+          int flipRow = size - 1 - col;
+          int flipcol = size - 1 - row;
+
+          swap(row, col, flipRow, flipcol);
+        }
+      }
+    }
+  }
 }
--- matrix/RunMatrix.java
+++ matrix/RunMatrix.java
@@ -1,4 +1,33 @@
+import java.util.Scanner;
+
 public class RunMatrix {
   public static void main(String[] args) {
+  Scanner scanner = new Scanner(System.in);
+
+  System.out.print("Please enter the size of your matrix: ");
+  int size = scanner.nextInt();
+
+  System.out.println("\nYour matrix is " + size + "x" + size + "\n");
+
+  Matrix m = new Matrix(size);
+
+  System.out.println("Printing matrix with default values:");
+  m.printDefultMatrix();
+
+  System.out.print("\nPopulating matrix...");
+  m.populateMatrix();
+  System.out.println("matrix populated\n");
+
+  System.out.println("Printing matrix:");
+  m.printMatrix();
+
+  System.out.println("Flipping matrix...\n");
+  m.flipMatrix();
+  System.out.println("matrix flipped\n");
+  m.printMatrix();
+
+
+  scanner.close();
+
   }
 }
